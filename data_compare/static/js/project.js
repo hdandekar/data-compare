1efb6d20ac90f7db89cc730ba871fc938766227c
Hiresh Dandekar <2361550+hdandekar@users.noreply.github.com>
2024-01-26 18:22:55 +0530

    Minor UI Changes (#3)

--- data_compare/static/js/project.js
+++ data_compare/static/js/project.js
@@ -1,10 +1,10 @@
 
+
 // Enable tooltip
-// TODO: Tooltip does not work on edit/delete icons
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
-(function(){
+(function () {
   console.log("Hello World!")
 })();
