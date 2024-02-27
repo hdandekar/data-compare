661afed2eadb142900b4a1c32d935e79ad3182d6
Hiresh Dandekar <2361550+hdandekar@users.noreply.github.com>
2024-02-27 16:20:19 +0530

    Fixed Docker and production.txt for deployment failures Fix user avatar link Fix import error

--- config/settings/production.py
+++ config/settings/production.py
@@ -77,7 +77,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 # MEDIA
