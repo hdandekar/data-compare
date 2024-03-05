e4f5c74280258a7a18155a8b37a4110ffafe8a2b
Hiresh Dandekar <2361550+hdandekar@users.noreply.github.com>
2024-03-05 10:55:07 +0530

    Pagination (#8)
    * Added pagination for Connection, Project, Module and TestCase objects

--- testplan/models.py
+++ testplan/models.py
@@ -54,6 +54,7 @@ class Module(models.Model):
     modified_by = models.ForeignKey(
         User,
         related_name="module_modified_by",
+        null=True,
         on_delete=models.SET(get_deleted_user_instance),
     )
 
@@ -65,27 +66,24 @@ class TestCase(models.Model):
     tcname = models.CharField(max_length=50, blank=False, null=False)
     project = models.ForeignKey(Project, related_name="project_testcases", on_delete=models.PROTECT)
     module = models.ForeignKey(Module, related_name="module_testcases", on_delete=models.PROTECT)
-    sourcedb = models.ForeignKey(DbConnection, null=True,
-                                   on_delete=models.PROTECT,
-                                   related_name='sourcedb')
+    sourcedb = models.ForeignKey(DbConnection, null=True, on_delete=models.PROTECT, related_name="sourcedb")
     sourcesql = models.TextField(max_length=None, null=False, blank=False)
-    targetdb = models.ForeignKey(DbConnection, null=True,
-                                   on_delete=models.PROTECT,
-                                   related_name='targetdb')
+    targetdb = models.ForeignKey(DbConnection, null=True, on_delete=models.PROTECT, related_name="targetdb")
     targetsql = models.TextField(max_length=None, null=False, blank=False)
     keycolumns = models.TextField(max_length=None, default="All")
     create_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(
-                                    User,
-                                    related_name="testcase_created_by",
-                                    on_delete=models.SET(get_deleted_user_instance),
-                                )
+        User,
+        related_name="testcase_created_by",
+        on_delete=models.SET(get_deleted_user_instance),
+    )
     modified_by = models.ForeignKey(
-                                    User,
-                                    related_name="testcase_modified_by", null=True,
-                                    on_delete=models.SET(get_deleted_user_instance),
-                                )
+        User,
+        related_name="testcase_modified_by",
+        null=True,
+        on_delete=models.SET(get_deleted_user_instance),
+    )
 
     def __str__(self):
         return str(self.id)
--- testplan/urls.py
+++ testplan/urls.py
@@ -5,21 +5,22 @@ from testplan import views
 urlpatterns = [
     # Project URLs
     path("projects", views.project_index, name="projects"),
-    path("project/", views.ProjectListView.as_view(), name="list_project"),
-    path("project/create/", views.ProjectCreateView.as_view(), name="create_project"),
-    path("project/<int:pk>/edit/", views.ProjectUpdateView.as_view(), name="edit_project"),
-    path("project/<int:pk>/delete/", views.ProjectDeleteView.as_view(), name="delete_project"),
+    path("project/list/<int:page>", views.ProjectListView.as_view(), name="list_project"),
+    path("project/create", views.ProjectCreateView.as_view(), name="create_project"),
+    path("project/<int:pk>/edit", views.ProjectUpdateView.as_view(), name="edit_project"),
+    path("project/<int:pk>/delete", views.ProjectDeleteView.as_view(), name="delete_project"),
     path("project/<int:pk>", views.ProjectDetailView.as_view(), name="project_details"),
-
     # Module URLs
-    path("project/<int:project_id>/modules", views.ModuleListView.as_view(), name="list_modules"),
+    path("project/<int:project_id>/module/<int:page>", views.ModuleListView.as_view(), name="list_module"),
     path("project/<int:project_id>/module/create", views.ModuleCreateView.as_view(), name="create_module"),
     path("project/<int:project_id>/module/<int:module_id>/edit", views.ModuleUpdateView.as_view(), name="edit_module"),
     path("project/<int:project_id>/module/<int:module_id>/delete", views.module_delete, name="delete_module"),
-
     # Test Case URLs
     path("project/<int:project_id>/testcase/create", views.TestCaseCreateView.as_view(), name="create_testcase"),
     path("project/<int:project_id>/testcases", views.TestCaseListView.as_view(), name="list_testcase"),
-    path("project/<int:project_id>/testcase/<int:testcase_id>/edit", 
-         views.TestCaseUpdateView.as_view(), name="edit_testcase"),
+    path(
+        "project/<int:project_id>/testcase/<int:testcase_id>/edit",
+        views.TestCaseUpdateView.as_view(),
+        name="edit_testcase",
+    ),
 ]
