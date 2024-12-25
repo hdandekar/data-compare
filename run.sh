319a9d4a99c6a395f76859b1fefb3c5b4767321e
Hiresh Dandekar <2361550+hdandekar@users.noreply.github.com>
2024-12-25 13:03:10 +0530

    Another try

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-celery -A data_compare worker -l INFO --concurrency=2 &
+#!/bin/sh
+
+
+# Start Gunicorn
 gunicorn --bind :${PORT} --workers 2 config.wsgi
+
+# Start Celery worker
+celery -A data_compare worker -l INFO --concurrency=2 &
